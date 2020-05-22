fd8571652ef3d19842fca9108ee4e5f891d31daa
romson <romerolijkwan@gmail.com>
2020-05-22 17:43:17 +0200

    first commit

---
+++ precache-manifest.a5c21d17c490c2cefd40727226216a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20c4d17e8e8d38f90cf33ecd089b3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e68792e7f29450ee964",
+    "url": "/static/css/main.a4f5a4e2.chunk.css"
+  },
+  {
+    "revision": "19d6e33bfe221ec5eea4",
+    "url": "/static/js/2.b0b70373.chunk.js"
+  },
+  {
+    "revision": "76337fd691e4155561d496e490beeac9",
+    "url": "/static/js/2.b0b70373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e68792e7f29450ee964",
+    "url": "/static/js/main.c0dbc060.chunk.js"
+  },
+  {
+    "revision": "d5952b8d3268e545475b",
+    "url": "/static/js/runtime-main.fd9eba62.js"
+  }
+]);
